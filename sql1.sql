1427b6c138de5e4f3c79f339ee3d860d1daebb0d
dimtsakiris <44496077+dimtsakiris@users.noreply.github.com>
2018-10-26 14:50:10 +0300

    update file

--- sql1.sql
+++ sql1.sql
@@ -3,3 +3,11 @@ INNER JOIN OrderDetails ON Products.ProductID = OrderDetails.ProductID
 INNER JOIN Orders ON Orders.OrderID = OrderDetails.OrderID
 INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
 WHERE City = 'Madrid'
+
+
+
+SELECT * FROM [Products]
+INNER JOIN OrderDetails ON Products.ProductID = OrderDetails.ProductID
+INNER JOIN Orders ON Orders.OrderID = OrderDetails.OrderID
+INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+WHERE City = 'Madrid'
