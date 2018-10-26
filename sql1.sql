1a139155794708f7d2301ad1ecd30c704cd5b9b1
TSAKIRIS Fotis <ftsakiris@intrasoft-intl.com>
2018-10-26 14:45:39 +0300

    new sql file

---
+++ sql1.sql
@@ -0,0 +1,5 @@
+SELECT sum(OrderDetails.Quantity) FROM [Products]
+INNER JOIN OrderDetails ON Products.ProductID = OrderDetails.ProductID
+INNER JOIN Orders ON Orders.OrderID = OrderDetails.OrderID
+INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+WHERE City = 'Madrid'
